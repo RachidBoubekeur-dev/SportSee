210cda7a98a2f9f7de6f216b0cdc168da5f9b832
Boubekeur <rachidboud>
2021-07-13 23:37:56 +0200

    :hammer: development component App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,12 @@
-import logo from '../assets/logo.png';
 import '../styles/css/App.css';
+import Header from './Header';
 
-function App() {
+const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <main className="App">
+            <Header />
+        </main>
     );
-}
+};
 
 export default App;
