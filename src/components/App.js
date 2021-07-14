f57da4b455ef664b88aeee553ab774b9c44a81b8
Boubekeur <rachidboud>
2021-07-14 06:48:37 +0200

    :lipstick: development UI loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
+import { useState, useEffect } from 'react';
 import '../styles/css/App.css';
 import Header from './Header';
+import imgLoading from '../assets/loading.gif';
+import logo from '../assets/logo2.png';
 
 const App = () => {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 3500);
+    }, []);
+
     return (
         <main className="App">
+            {loading && (
+                <div className="divLoading">
+                    <p>
+                        <img src={logo} alt="Logo SportSee" />
+                    </p>
+                    <span>
+                        <img src={imgLoading} alt="Chargement de la page" />
+                    </span>
+                </div>
+            )}
             <Header />
         </main>
     );
