1d79e637f5335e4152f66f54bcd9fd5f698f4415
Boubekeur <rachidboud>
2021-07-15 12:12:44 +0200

    :lipstick: :iphone: development UI and responsive design for component Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,14 @@
 import '../styles/css/Dashboard.css';
 
 const Dashboard = () => {
-    return 'Bonjour Thomas';
+    return (
+        <section className="Dashboard">
+            <h1>
+                Bonjour <span>Thomas</span>
+            </h1>
+            <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+        </section>
+    );
 };
 
 export default Dashboard;
