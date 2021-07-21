9f860e760320dd379426bb63e966c22c7efaeee8
Boubekeur <rachidboud>
2021-07-21 21:13:30 +0200

    :hammer: development script recharts in UserActivity component

--- API/app/data.js
+++ API/app/data.js
@@ -11,8 +11,8 @@ const USER_MAIN_DATA = [
             calorieCount: 1930,
             proteinCount: 155,
             carbohydrateCount: 290,
-            lipidCount: 50
-        }
+            lipidCount: 50,
+        },
     },
     {
         id: 18,
@@ -26,94 +26,93 @@ const USER_MAIN_DATA = [
             calorieCount: 2500,
             proteinCount: 90,
             carbohydrateCount: 150,
-            lipidCount: 120
-        }
-    }
-]
+            lipidCount: 120,
+        },
+    },
+];
 
 const USER_ACTIVITY = [
     {
         userId: 12,
         sessions: [
             {
-                day: '2020-07-01',
+                day: '01-07-2020',
                 kilogram: 80,
-                calories: 240
+                calories: 240,
             },
             {
-                day: '2020-07-02',
+                day: '02-07-2020',
                 kilogram: 80,
-                calories: 220
+                calories: 220,
             },
             {
-                day: '2020-07-03',
+                day: '03-07-2020',
                 kilogram: 81,
-                calories: 280
+                calories: 280,
             },
             {
-                day: '2020-07-04',
+                day: '04-07-2020',
                 kilogram: 81,
-                calories: 290
+                calories: 290,
             },
             {
-                day: '2020-07-05',
+                day: '05-07-2020',
                 kilogram: 80,
-                calories: 160
+                calories: 160,
             },
             {
-                day: '2020-07-06',
+                day: '06-07-2020',
                 kilogram: 78,
-                calories: 162
+                calories: 162,
             },
             {
-                day: '2020-07-07',
+                day: '07-07-2020',
                 kilogram: 76,
-                calories: 390
-            }
-        ]
+                calories: 390,
+            },
+        ],
     },
     {
         userId: 18,
         sessions: [
             {
-                day: '2020-07-01',
+                day: '01-07-2020',
                 kilogram: 70,
-                calories: 240
+                calories: 240,
             },
             {
-                day: '2020-07-02',
+                day: '02-07-2020',
                 kilogram: 69,
-                calories: 220
+                calories: 220,
             },
             {
-                day: '2020-07-03',
+                day: '03-07-2020',
                 kilogram: 70,
-                calories: 280
+                calories: 280,
             },
             {
-                day: '2020-07-04',
+                day: '04-07-2020',
                 kilogram: 70,
-                calories: 500
+                calories: 500,
             },
             {
-                day: '2020-07-05',
+                day: '05-07-2020',
                 kilogram: 69,
-                calories: 160
+                calories: 160,
             },
             {
-                day: '2020-07-06',
+                day: '06-07-2020',
                 kilogram: 69,
-                calories: 162
+                calories: 162,
             },
             {
-                day: '2020-07-07',
+                day: '07-07-2020',
                 kilogram: 69,
-                calories: 390
-            }
-        ]
-    }
-]
-
+                calories: 390,
+            },
+        ],
+    },
+];
 
 const USER_AVERAGE_SESSIONS = [
     {
@@ -121,69 +120,68 @@ const USER_AVERAGE_SESSIONS = [
         sessions: [
             {
                 day: 1,
-                sessionLength: 30
+                sessionLength: 30,
             },
             {
                 day: 2,
-                sessionLength: 23
+                sessionLength: 23,
             },
             {
                 day: 3,
-                sessionLength: 45
+                sessionLength: 45,
             },
             {
                 day: 4,
-                sessionLength: 50
+                sessionLength: 50,
             },
             {
                 day: 5,
-                sessionLength: 0
+                sessionLength: 0,
             },
             {
                 day: 6,
-                sessionLength: 0
+                sessionLength: 0,
             },
             {
                 day: 7,
-                sessionLength: 60
-            }
-        ]
+                sessionLength: 60,
+            },
+        ],
     },
     {
         userId: 18,
         sessions: [
             {
                 day: 1,
-                sessionLength: 30
+                sessionLength: 30,
             },
             {
                 day: 2,
-                sessionLength: 40
+                sessionLength: 40,
             },
             {
                 day: 3,
-                sessionLength: 50
+                sessionLength: 50,
             },
             {
                 day: 4,
-                sessionLength: 30
+                sessionLength: 30,
             },
             {
                 day: 5,
-                sessionLength: 30
+                sessionLength: 30,
             },
             {
                 day: 6,
-                sessionLength: 50
+                sessionLength: 50,
             },
             {
                 day: 7,
-                sessionLength: 50
-            }
-        ]
-    }
-]
-
+                sessionLength: 50,
+            },
+        ],
+    },
+];
 
 const USER_PERFORMANCE = [
     {
@@ -194,34 +192,34 @@ const USER_PERFORMANCE = [
             3: 'endurance',
             4: 'strength',
             5: 'speed',
-            6: 'intensity'
+            6: 'intensity',
         },
         data: [
             {
                 value: 80,
-                kind: 1
+                kind: 1,
             },
             {
                 value: 120,
-                kind: 2
+                kind: 2,
             },
             {
                 value: 140,
-                kind: 3
+                kind: 3,
             },
             {
                 value: 50,
-                kind: 4
+                kind: 4,
             },
             {
                 value: 200,
-                kind: 5
+                kind: 5,
             },
             {
                 value: 90,
-                kind: 6
-            }
-        ]
+                kind: 6,
+            },
+        ],
     },
     {
         userId: 18,
@@ -231,42 +229,40 @@ const USER_PERFORMANCE = [
             3: 'endurance',
             4: 'strength',
             5: 'speed',
-            6: 'intensity'
+            6: 'intensity',
         },
         data: [
             {
                 value: 200,
-                kind: 1
+                kind: 1,
             },
             {
                 value: 240,
-                kind: 2
+                kind: 2,
             },
             {
                 value: 80,
-                kind: 3
+                kind: 3,
             },
             {
                 value: 80,
-                kind: 4
+                kind: 4,
             },
             {
                 value: 220,
-                kind: 5
+                kind: 5,
             },
             {
                 value: 110,
-                kind: 6
-            }
-        ]
-    }
-]
-
-
+                kind: 6,
+            },
+        ],
+    },
+];
 
 module.exports = {
     USER_MAIN_DATA,
     USER_ACTIVITY,
     USER_AVERAGE_SESSIONS,
-    USER_PERFORMANCE
-}
+    USER_PERFORMANCE,
+};
--- src/components/smart/UserActivity.jsx
+++ src/components/smart/UserActivity.jsx
@@ -1,15 +1,59 @@
 import { useContext } from 'react';
 import { IdContext } from '../context/MyContext.jsx';
 import useAxios from '../hook/useAxios';
+import {
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    Tooltip,
+    Legend,
+    CartesianGrid,
+} from 'recharts';
 
 const UserActivity = () => {
     const userId = useContext(IdContext);
-    console.log(userId);
     const userActivity = useAxios(`user/${userId}/activity`);
-    console.log(userActivity);
+    if (userActivity) {
+        for (let i = 0; i < userActivity.sessions.length; i++) {
+            const day = userActivity.sessions[i].day.toString();
+            if (day.charAt(0) === '0')
+                userActivity.sessions[i].day = day.slice(1, 2);
+            else userActivity.sessions[i].day = day.slice(0, 2);
+        }
+    }
     return (
         <article>
-            <p>UserActivity</p>
+            {userActivity ? (
+                <BarChart width={600} height={300} data={userActivity.sessions}>
+                    <XAxis dataKey="day" />
+                    <YAxis />
+                    <Tooltip
+                        wrapperStyle={{
+                            width: 100,
+                            backgroundColor: '#ccc',
+                        }}
+                    />
+                    <Legend
+                        width={100}
+                        wrapperStyle={{
+                            top: 40,
+                            right: 20,
+                            backgroundColor: '#f5f5f5',
+                            border: '1px solid #d5d5d5',
+                            borderRadius: 3,
+                            lineHeight: '40px',
+                        }}
+                    />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <Bar dataKey="kilogram" fill="#000" barSize={10} />
+                    <Bar dataKey="calories" fill="#E60000" barSize={10} />
+                </BarChart>
+            ) : (
+                <div>
+                    <p>Charement...</p>
+                </div>
+            )}
         </article>
     );
 };
