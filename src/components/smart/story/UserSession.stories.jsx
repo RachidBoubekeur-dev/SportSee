a4585bc1e82c1c792a0b332813c666daf84e089e
Boubekeur <rachidboud>
2021-07-28 19:43:29 +0200

    💡 develop comments and stories for UserSession smart component

--- src/components/smart/story/UserSession.stories.jsx
+++ src/components/smart/story/UserSession.stories.jsx
@@ -4,17 +4,29 @@ import UserSession from '../UserSession';
 export default {
     title: 'smart/UserSession',
     component: UserSession,
+    argTypes: {
+        user: {
+            options: {
+                Karl: '12',
+                Cecilia: '18',
+            },
+            defaultValue: '12',
+            description: 'Default user on SportSee',
+            control: { type: 'radio' },
+        },
+    },
 };
 
-export const Default = () => {
+export const Session = ({ user }) => {
     const divStyle = {
         display: 'flex',
-        width: '60%',
-        height: '210px',
+        width: '100%',
+        maxWidth: '1000px',
+        height: '270px',
     };
     return (
         <div style={divStyle}>
-            <IdContext.Provider value={'18'}>
+            <IdContext.Provider value={user}>
                 <UserSession />
             </IdContext.Provider>
         </div>
